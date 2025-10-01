b0e637952cb941de2b3594ee04a692bdb07acdad
anajubs0925-glitch <ana.jubs0925@gmail.com>
2025-10-01 16:20:41 -0300

    Add files via upload

---
+++ julia.py
@@ -0,0 +1,8 @@
+idade= int (input("digite sua idade"))
+if idade < 18: 
+    print("voce e menor de idade")
+elif 18 <= idade < 60:
+    print("voce e adulto")
+else: 
+    print("voce e idoso")
+    
